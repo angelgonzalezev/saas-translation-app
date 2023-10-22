f0fb23635647e6ca70f78dfa41fe250d4023de97
Angel Gonzalez <aangel.gg@hotmail.com>
2023-10-22 18:08:12 +0200

    First commit

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import LogoImage from '@logos/logo.svg'
+import Link from 'next/link'
+
+const Logo = () => {
+  return (
+    <Link href={'/'}>
+        Logo
+    </Link>
+  )
+}
+export default Logo
---
+++ sections/Header.tsx
@@ -0,0 +1,13 @@
+import Logo from "@/components/Logo"
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            {/* Logo */}
+            <Logo />
+        </nav>
+    </header>
+  )
+}
+export default Header
